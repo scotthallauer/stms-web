a6f0260a63d822a42e68e2966c39ee168997e747
Scott <scott.hallauer@gmail.com>
2018-08-29 14:08:15 +0200

    Dramatically simplified Priority class
    It's static now... No need to create instances because it's a once-off calculation.

--- src/com/stms/web/Priority.java
+++ src/com/stms/web/Priority.java
@@ -2,126 +2,49 @@ package com.stms.web;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.time.temporal.ChronoUnit;
 import java.time.*;
-import java.text.*;
 import java.lang.Math;
 
 /**
  * This class takes a course session ID as the parameter and calculates the prioDuerity for the session.
  *
- * Coded by Jonathon Everatt
+ * Coded by Scott Hallauer and Jonathon Everatt
  */
 
 public class Priority {
-
-    private Double priority;
-    private Double prioWeight;
-    private Double prioritylevel;
-    private int assignmentID;
-    private Database DB;
-    private Date today;
-    private Utilities Util;
-
-    /**
-     *  Constructor for the class. Initializes  Date Utils and the prioDuerity level
-     */
-    Priority() {
-        this.prioritylevel = 0.0;
-        Util = new Utilities();
-
-    }
 
     /**
      *  Main method for the class takes in the Due date and calculates the amount of days till it is due
      *  Then assigns a prioDuerity level to the duedate and returns.
      *
-     * @param due Due date of the assignment that needs prioDuerity calculated
+     * @param dueDate Due date of the assignment that needs prioDuerity calculated
      * @return The prioDuerity for the assignment on curve 100x^0.5 or 150 if due in less than 2 days
      */
-    public double CalcPriority(LocalDate due, String priorityLevel, Double Weighting){
-		
-		if (Weighting == null){
-            return CalcPriority(due, priorityLevel);
+    public static double calculate(LocalDate dueDate, Integer userPriority, Double weighting){
+
+        long daysLeft = ChronoUnit.DAYS.between(LocalDate.now(), dueDate);
+
+        double datePriority;
+        if(daysLeft <= 2){
+            datePriority = 100.0;
+        }else{
+            datePriority = 100.0 * Math.pow(daysLeft - 1, -0.387); // = 100 * (daysLeft-1)^-0.387
         }
 
-        setPriorityLevel(priorityLevel);
-
-        setWeigthing(Weighting);
-
-        int daysLeft = Util.calcDayNumInYear(due) - Util.calcDayNumInYear(Util.getDateToday());
-        System.out.println(daysLeft + " is the amount of days left");
-        if(daysLeft < 2){
-            //Max absolutely top level priority must be completed
-            return 100.0;
+        if(userPriority == null){
+            userPriority = 2; // default to 'normal' priority
         }
 
-        double prioDue;
-        //y = 100(daysUntil -1) ^-.387
-        prioDue = daysLeft - 1;
-        //prioDue *= 100;
-        prioDue = Math.pow(prioDue, -0.387);
-        prioDue *= 100;
-
-        double prio = (0.65 * prioDue) + (20 * prioritylevel) + (0.15 * prioWeight);
-        //Current equation = 100x^1/2 + prioDuelevel
-        return prio;
-    }
-
-    public double CalcPriority(LocalDate due, String priorityLevel){
-        setPriorityLevel(priorityLevel);
-        int daysLeft = Util.calcDayNumInYear(due) - Util.calcDayNumInYear(Util.getDateToday());
-
-        double prioDue;
-        prioDue = daysLeft - 1;
-
-        prioDue = Math.pow(prioDue, -0.387);
-        prioDue *= 100;
-
-        double prio = (0.70 * prioDue) + (30 * prioritylevel);
-
-        //Current equation = 100x^1/2 + prioDuelevel
-        return  prio;
-    }
-
-    /**
-     * Set and get methods for the class
-     *
-     */
-
-    public void setWeigthing(Double prioWeight){
-        if(prioWeight < 1.0){
-            this.prioWeight = prioWeight * 100;
-        } else {
-            this.prioWeight = prioWeight; //In case it is entered as a decimal
+        double overallPriority;
+        if(weighting == null){
+            overallPriority = (0.7 * (datePriority/100.0)) + (0.3 * (userPriority/3.0));
+        }else{
+            overallPriority = (0.65 * (datePriority/100.0)) + (0.2 * (userPriority/3.0)) + (0.15 * weighting/100.0);
         }
+
+        return overallPriority * 100.0;
+
     }
 
-    public void setPriorityLevel(String level){ //If easier can set this to int with levels of 1,2,3
-        if(level.equals("High")) {
-            this.prioritylevel = 1.0;
-        } else if (level.equals("Medium")) {
-            this.prioritylevel = 0.66667;
-        }  else if (level.equals("Low")) {
-            this.prioritylevel = 0.33334;
-        } else{
-            System.out.println("Not priority  level set. Invalid input setPrio ");
-        }
-    }
-
-    public void setPriority(Double priority){
-        this.priority = priority;
-    }
-
-    public void setassignmentID(int assignmentID){
-        this.assignmentID = assignmentID;
-    }
-
-    public Double getPriority(){
-        return priority;
-    }
-
-    public int getassignmentID(){
-        return  assignmentID;
-    }
 }
