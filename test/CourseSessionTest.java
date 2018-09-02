88f65860d20389ac3fb11477b155cf6ea6af9782
KarmaZA <39301642+KarmaZA@users.noreply.github.com>
2018-09-02 19:20:23 +0200

    Haven't fully completed the tests
    I'm hungry. Getting food, watching the group chat getting lit

--- test/CourseSessionTest.java
+++ test/CourseSessionTest.java
@@ -6,8 +6,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CourseSessionTest {
 
+    private CourseSession CS1;
+    private CourseSession CS2;
+    private CourseSession CS3;
+
     @BeforeEach
     void setUp() {
+        try{
+            CS1 = new CourseSession(1);
+            CS2 = new CourseSession(8);
+            CS3 = new CourseSession(10);
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @AfterEach
@@ -16,6 +28,8 @@ class CourseSessionTest {
 
     @Test
     void getSessionID() {
+        int x = CS1.getSessionID();
+        assertEquals(1,x);
     }
 
     @Test
@@ -24,6 +38,10 @@ class CourseSessionTest {
 
     @Test
     void getSessionPID() {
+        int ID = CS2.getSessionPID();
+        int ID2 = CS1.getSessionPID();
+        assertEquals(8, ID);
+        assertEquals(0, ID2);
     }
 
     @Test
@@ -32,6 +50,8 @@ class CourseSessionTest {
 
     @Test
     void getCourseID() {
+        int x = CS3.getCourseID();
+        assertEquals(3,x);
     }
 
     @Test
@@ -40,6 +60,8 @@ class CourseSessionTest {
 
     @Test
     void getType() {
+        String s = CS1.getType();
+        assertEquals("lecture", s);
     }
 
     @Test
@@ -64,38 +86,59 @@ class CourseSessionTest {
 
     @Test
     void getLength() {
+
     }
 
     @Test
     void setRecType() {
+
     }
 
     @Test
     void getRecType() {
+        String rec = CS1.getRecType();
+        assertEquals("week_1___1,2,3,4,5#", rec);
     }
 
     @Test
     void setLocation() {
+        CS1.setLocation("Menzies 5");
+        assertNotNull(CS1.getLocation());
+        assertEquals("Menzies 5", CS1.getLocation());
+        CS1.setLocation(null);
     }
 
     @Test
     void getLocation() {
+        String location = CS1.getLocation();
+        assertNull(location);
     }
 
     @Test
     void setNote() {
+        CS3.setNote("Test");
+        assertNotNull(CS3.getNote());
+        assertEquals("Test",CS3.getNote());
     }
 
     @Test
     void getNote() {
+        String s = CS1.getType();
+        assertNull(s);
+
     }
 
     @Test
     void setPriority() {
+        CS1.setPriority(10);
+        assertEquals(10, CS3.getPriority());
+        CS1.setPriority(null);
     }
 
     @Test
     void getPriority() {
+        int prio = CS3.getPriority();
+        assertEquals(3, prio);
     }
 
     @Test
--- test/CourseTest.java
+++ test/CourseTest.java
@@ -6,8 +6,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CourseTest {
 
+    private Course course1;
+    private Course course2;
+    private Course course3;
+
     @BeforeEach
     void setUp() {
+        try {
+            course1 = new Course(1);
+            course2 = new Course(4);
+            course3 = new Course(7);
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("All tests are invalid");
+        }
     }
 
     @AfterEach
@@ -16,18 +28,24 @@ class CourseTest {
 
     @Test
     void getSessions() {
+        assertNotNull(course1.getSessions());
     }
 
     @Test
     void getGradedSessions() {
+        assertNotNull(course1.getGradedSessions());
     }
 
     @Test
     void getCourseID() {
+        int x1 = course1.getCourseID();
+        assertEquals(x1, 1);
     }
 
     @Test
     void getSemesterID1() {
+        int semID1 = course2.getSemesterID1();
+        assertEquals(semID1, 2);
     }
 
     @Test
@@ -36,6 +54,8 @@ class CourseTest {
 
     @Test
     void getSemesterID2() {
+        int test = course3.getSemesterID2();
+        assertNull(test);
     }
 
     @Test
@@ -44,6 +64,8 @@ class CourseTest {
 
     @Test
     void getName() {
+        String s = course3.getName();
+        assertEquals(s, "Computer Science");
     }
 
     @Test
@@ -52,6 +74,8 @@ class CourseTest {
 
     @Test
     void getCode() {
+        String code = course1.getCode();
+        assertEquals(code, "CSC3002F");
     }
 
     @Test
