88aa34892c586efba7745db0c65cb0b9c277cdc6
KarmaZA <39301642+KarmaZA@users.noreply.github.com>
2018-09-02 18:03:20 +0200

    SemesterTest Done
    I hate JUniut. TBH this is going slowly because I want to smash my face against a doritos packet

--- test/SemesterTest.java
+++ test/SemesterTest.java
@@ -2,12 +2,28 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SemesterTest {
 
+    private Semester semester1;
+    private Semester semester2;
+    private Semester semester3;
+
     @BeforeEach
     void setUp() {
+        try {
+            semester1 = new Semester(1);
+            semester2 = new Semester(4);
+            semester3 = new Semester(5);
+        } catch (Exception e){
+             e.printStackTrace();
+        }
+
     }
 
     @AfterEach
@@ -20,26 +36,47 @@ class SemesterTest {
 
     @Test
     void getSemesterID() {
+        int ID1 = semester1.getSemesterID();
+        int ID2 = semester2.getSemesterID();
+        assertNotEquals(ID1, ID2);
+        assertEquals(ID1, 1);
+        assertEquals(ID2, 4);
     }
 
     @Test
     void getUserID() {
+        int userID1 = semester1.getUserID();
+        int userID2  = semester3.getUserID();
+        assertEquals(1, userID1);
+        assertEquals(3, userID2);
     }
 
     @Test
     void setUserID() {
+        semester3.setUserID(1);
+        assertEquals(semester1.getUserID(), semester3.getUserID());
+        semester3.setUserID(3);
     }
 
     @Test
     void getName() {
+        String s = semester1.getName();
+        assertEquals("1st Semester", s);
     }
 
     @Test
     void setName() {
+        semester3.setName("This is a test");
+        assertEquals("This is a test", semester3.getName());
+        semester3.setName("1st Semester");
     }
 
     @Test
     void getStartDate() {
+        LocalDateTime temp = LocalDateTime.of(2018, 01, 19, 0, 0);
+        Timestamp start = Timestamp.valueOf(temp);
+        Timestamp check = semester1.getStartDate();
+        assertEquals(start, check);
     }
 
     @Test
@@ -48,6 +85,10 @@ class SemesterTest {
 
     @Test
     void getEndDate() {
+        LocalDateTime temp = LocalDateTime.of(2018, 06, 24, 0, 0);
+        Timestamp end = Timestamp.valueOf(temp);
+        Timestamp check = semester1.getEndDate();
+        assertEquals(end, check);
     }
 
     @Test
