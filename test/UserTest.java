22dd9d06eb13c637bcf78bff270e7abf9f082c96
KarmaZA <39301642+KarmaZA@users.noreply.github.com>
2018-09-02 16:41:58 +0200

    Basic UserTest class completed

--- test/UserTest.java
+++ test/UserTest.java
@@ -6,8 +6,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
+    User user1;
+    User user2;
+    User user3;
+
     @BeforeEach
     void setUp() {
+        try{
+            user1 = new User("scott.hallauer@gmail.com");
+            user2 = new User("EVRJON003@myuct.ac.za");
+            user3 = new User("BRNJES018@myuct.ac.za");
+        } catch (Exception e){
+            System.out.println("Failed User Test creation. Tests invalid");
+            e.printStackTrace();
+        }
+
+
     }
 
     @AfterEach
@@ -20,34 +34,67 @@ class UserTest {
 
     @Test
     void getUserID() {
+        int x = user1.getUserID();
+        assertEquals(1, x);
+    }
+
+    @Test
+    void FgetUserID() {
+        int x = user1.getUserID();
+        int y = user2.getUserID();
+        assertNotEquals(x, y);
     }
 
     @Test
     void getFirstName() {
+        String s = user2.getFirstName();
+        assertEquals(s, "Jonathon");
     }
 
     @Test
     void setFirstName() {
+        user2.setFirstName("Steve");
+        String s = user2.getFirstName();
+        assertEquals("Steve", s);
+        user2.setFirstName("Jonathon");
+        s = user2.getFirstName();
+        assertEquals(s, "Jonathon");
     }
 
     @Test
     void getLastName() {
+        String s = user3.getLastName();
+        assertEquals("Bourn", s);
     }
 
     @Test
     void setLastName() {
+        user3.setLastName("JeanB");
+        assertEquals("JeanB", user3.getLastName());
+        user3.setLastName("Bourn");
     }
 
     @Test
     void getInitials() {
+        String s1 = user2.getInitials();
+        String s2 = user1.getInitials();
+        assertEquals("JE", s1);
+        assertEquals("SH", s2);
+        assertNotEquals(s1,s2);
     }
 
     @Test
     void getEmail() {
+        String s = user2.getEmail();
+        assertEquals("EVRJON003@myuct.ac.za", s);
     }
 
     @Test
     void setEmail() {
+        user1.setEmail("HLLSCO002@myuct.ac.za");
+        String s = user1.getEmail();
+        user1.setEmail("scott.hallauer@gmail.com");
+        assertEquals("HLLSCO002@myuct.ac.za", s);
     }
 
     @Test
@@ -72,10 +119,14 @@ class UserTest {
 
     @Test
     void isActivated() {
+        assertTrue(user1.isActivated());
     }
 
     @Test
     void setActivated() {
+        user3.setActivated(false);
+        assertFalse(user3.isActivated());
+        user3.setActivated(true);
     }
 
     @Test
